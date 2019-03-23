bf5290626e0be0cbe0e299b0f4e685f169256221
Brandon Sam Soon <10151952+brsmsn@users.noreply.github.com>
2019-03-23 14:42:04 -0400

    challenge 11 done

--- README.md
+++ README.md
@@ -1,15 +1,15 @@
 # cryptopals
-This repo host my solutions to the Matasano Crypto Challege otherwise known as cryptopals.
+This repo host my solutions to the Matasano Crypto Challenges otherwise known as cryptopals.
 
 
 ## Progress
 | |Ch 1|Ch 2|Ch 3|Ch 4|Ch 5|Ch 6|Ch 7|Ch 8|
-|---|:---:|:---:|:---:|:---:|:---:|:---:|:---:|:---:| 
-|Set 1| X |X|X|X|X|X|X|X| 
-|Set 2|X|X|   |   |   |   |   |   |
-|Set 3|   |   |   |   |   |   |   |   |
-|Set 4|   |   |   |   |   |   |   |   |
-|Set 5|   |   |   |   |   |   |   |   |
-|Set 6|   |   |   |   |   |   |   |   |
-|Set 7|   |   |   |   |   |   |   |   |
-|Set 8|   |   |   |   |   |   |   |   |
+|:---:|:---:|:---:|:---:|:---:|:---:|:---:|:---:|:---:| 
+|Set 1|X|X|X|X|X|X|X|X| 
+|Set 2|X|X|X| | | | | |
+|Set 3| | | | | | | | |
+|Set 4| | | | | | | | |
+|Set 5| | | | | | | | |
+|Set 6| | | | | | | | |
+|Set 7| | | | | | | | |
+|Set 8| | | | | | | | |
--- set-2/challenge-11/oracle.go
+++ set-2/challenge-11/oracle.go
@@ -23,6 +23,96 @@ or CBC, tells you which one is happening. */
 
 package main
 
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"fmt"
+
+	"github.com/brsmsn/cryptopals/set-1/challenge-07/ecb"
+)
+
 func main() {
 
+	for i := 0; i < 10; i++ {
+		pt := []byte("asdasdasdasdasddskgjhdsfkgjbsdkjfhgbdsfjkhgbsdfjkghb")
+		ct := (encrypt(pt, genKey()))
+		fmt.Println("Encrypted with " + detectMode(ct))
+	}
+
+}
+
+func genKey() []byte {
+	key := make([]byte, 16)
+	rand.Read(key)
+	return key
+}
+
+func encrypt(pt, key []byte) []byte {
+	//generate  random amount of bytes from 5 to 10
+	genRand := func() []byte {
+		bytes := make([]byte, 1)
+		rand.Read(bytes)
+
+		//set bytes2 to an integer between 5 and 10 inclusive
+		bytes2 := make([]byte, bytes[0]%10+5)
+		rand.Read(bytes2)
+		return bytes2
+	}
+
+	pt1 := append(pt, genRand()...)
+	newPt := pad(append(genRand(), pt1...))
+	ct := make([]byte, len(newPt))
+
+	bytes := make([]byte, 1)
+	rand.Read(bytes)
+
+	block, _ := aes.NewCipher(key)
+
+	switch bytes[0] % 2 {
+	case 0:
+		mode := ecb.NewEBCDecrypter(block)
+		mode.CryptBlocks(ct, newPt)
+	case 1:
+		//encrrypt with cbc
+		iv := make([]byte, 16)
+		rand.Read(iv)
+		mode := cipher.NewCBCEncrypter(block, iv)
+		mode.CryptBlocks(ct, newPt)
+	}
+	return ct
+}
+
+//pad text to a multiple of 16 bytes
+func pad(text []byte) []byte {
+	blocksToPad := 16 - len(text)%16
+
+	if blocksToPad == 0 {
+		return text
+	}
+
+	padded := text
+	for i := 0; i < blocksToPad; i++ {
+		padded = append(padded, byte(blocksToPad))
+	}
+
+	return padded
+}
+
+func detectMode(ct []byte) string {
+	blocks := make(map[string]int)
+
+	for i := 0; i < len(ct); i += 16 {
+		block := string(ct[i : i+16])
+
+		//ECB has repeating blocks, a proper mode of operation would have no
+		//duplicate blocks
+		if _, isInMap := blocks[block]; isInMap {
+			return "ecb"
+		}
+
+		blocks[block] = 1
+	}
+
+	return "cbc"
 }
